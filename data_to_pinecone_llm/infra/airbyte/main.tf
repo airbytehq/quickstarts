6202c6d0b286c0e7df9785d9e8d0864928800fe7
Aaron (AJ) Steers <aj@airbyte.io>
2023-11-14 04:08:17 +0000

    rename connectors and connections, improve json creds handling

--- data_to_pinecone_llm/infra/airbyte/main.tf
+++ data_to_pinecone_llm/infra/airbyte/main.tf
@@ -1,14 +1,24 @@
 // Airbyte Terraform provider documentation: https://registry.terraform.io/providers/airbytehq/airbyte/latest/docs
 
+locals {
+  // The BigQuery destination expects a JSON credentials file, but the source expects a JSON string.
+  // Read JSON file into a string variable, so we can use the same contents also for the source connector.
+  bigquery_credentials_json = replace(
+    file(var.bigquery_credentials_json_file_path), "\n", ""
+  ) // <- If this fails, please double check that the file exists.
+
+  airbyte_cloud_url = var.airbyte_workspace_id != null ? "https://cloud.airbyte.com/workspaces/${var.airbyte_workspace_id}/connections" : "(n/a)"
+}
+
 // Sources
 resource "airbyte_source_bigquery" "bigquery" {
     configuration = {
-      credentials_json = var.bigquery_credentials_json
+      credentials_json = local.bigquery_credentials_json
       dataset_id       = var.bigquery_dataset_id
       project_id       = var.bigquery_project_id
       source_type      = "bigquery"
     }
-    name          = "My BigQuery source"
+    name          = "BigQuery Publishing Source"
     workspace_id  = var.airbyte_workspace_id
 }
 resource "airbyte_source_notion" "notion_source" {
@@ -22,7 +32,7 @@ resource "airbyte_source_notion" "notion_source" {
     source_type = "notion"
     start_date  = "2023-01-01T00:00:00.001Z"  # Note: Fractional seconds with .000 may not work.
   }
-  name         = "My Notion Source"
+  name         = "Notion Data Source"
   workspace_id = var.airbyte_workspace_id
 }
 
@@ -33,14 +43,14 @@ resource "airbyte_destination_bigquery" "bigquery" {
         dataset_location = var.bigquery_dataset_location
         destination_type = "bigquery"
         project_id = var.bigquery_project_id
-        credentials_json = var.bigquery_credentials_json_file_path
+        credentials_json = local.bigquery_credentials_json
         loading_method = {
             destination_bigquery_loading_method_standard_inserts = {
                 method = "Standard"
             }
         }
     }
-    name = "My BigQuery Destination"
+    name = "BigQuery Raw Data Destination"
     workspace_id = var.airbyte_workspace_id
 }
 resource "airbyte_destination_pinecone" "pinecone" {
@@ -64,7 +74,7 @@ resource "airbyte_destination_pinecone" "pinecone" {
       text_fields = ["notion_text"]
     }
   }
-  name          = "My Pinecone Destination"
+  name          = "Pinecone Publish Destination"
   workspace_id  = var.airbyte_workspace_id
 }
 
@@ -82,14 +92,14 @@ resource "airbyte_connection" "notion_connection" {
     }
 }
 resource "airbyte_connection" "bigquery_to_pinecone" {
-    name = "BigQuery to Pinecone"
+    name = "Publish BigQuery Data to Pinecone"
     source_id = airbyte_source_bigquery.bigquery.source_id
     destination_id = airbyte_destination_pinecone.pinecone.destination_id
     configurations = {
         streams = [
-            {
-              name         = "notion_data",
-            }
+            # {
+            #   name = "notion_data",
+            # }
         ]
     }
 }
